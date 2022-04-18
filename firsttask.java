8d0e3a779c1b0ee499a8c545f3e37974cc850ec6
porludom <porludom@mail.ru>
2022-04-18 13:04:14 +0300

    .

---
+++ firsttask.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class firsttask {
+    public static void main(String[] args) {
+
+    }
+}
+enum TEAM{
+    BARSELONA, MADRID
+}
+class FIFA{
+    ArrayList<Fan> madridFans = new ArrayList<>();
+    ArrayList<Fan> barselonaFans = new ArrayList<>();
+
+    public void startGame(TEAM team1, TEAM team2){
+        if(team1 == TEAM.BARSELONA || team2 == TEAM.BARSELONA){
+            barselonaFans.forEach(x -> x.sendMessage("Barselona game has started"));
+        }
+        if(team1 == TEAM.MADRID || team2 == TEAM.MADRID){
+            madridFans.forEach(x -> x.sendMessage("Madrid game has started"));
+        }
+    }
+}
+
+class Fan{
+public void sendMessage(String msg){
+
+}
+}
